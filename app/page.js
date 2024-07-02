7ec21ad2b0a4d9462ec72a6fe3ed8db9b2300f2c
Abdulmumin Isah <abdulmuminisah@yahoo.com>
2024-07-02 14:08:18 +0100

    inventory app

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-3x1 mb-4">Inventory Managment Software</h1>
       <Link href="/dashboard/home/overview/">View dashboard</Link>
       <Link href="/home/">View front end hompage</Link>
+      <h3>Currently Under Development</h3>
     </div>
   );
 }
